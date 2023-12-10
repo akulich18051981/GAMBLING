2e04d6d0f21256bc7a878bc9d10bbbadfcb6e381
Ольга Акулич <akulicholga81@gmail.com>
2023-12-10 18:01:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let header = document.getElementById("myBtn");
+let btns = header.getElementsByClassName(".buttonnav");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
